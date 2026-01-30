5b90f4eb08b6d671e18841543f0a3f1f0d7a771e
Aishwarya4400 <50081627+Aishwarya4400@users.noreply.github.com>
2026-01-30 15:40:01 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,12 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            <li> ENV_VAR: {process.env.ENV_VAR} </li>
-            <li> Key: {process.env.Key} </li> edit the page.tsx file.
+             edit the page.tsx file.
           </h1>
+          <ul>
+            <li> ENV_VAR: {process.env.ENV_VAR} </li>
+            <li> Key: {process.env.Key} </li>
+            </ul>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             change alert from personal!!! Looking for a starting point or more instructions? Head over to{" "}
             <a
